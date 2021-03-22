74ec4be7a18e60693f37b6ceed893aa936a561c6
Anna Kozak <anna1993kozak@gmail.com>
2021-03-22 20:36:21 +0100

    add lab 5

---
+++ Laboratoria/Lab_5.R
@@ -0,0 +1,75 @@
+#########################################
+###    WSTĘP DO EKSPLORACJI DANYCH    ###
+###         LABORATORIUM 5            ###
+#########################################
+
+library(dplyr)
+library(ggplot2)
+
+# Zadania z Lab 4
+
+# Zadanie 4
+# Ile jest samochodów poszczególnych marek, których rok produkcji jest 2008
+
+
+# Zadanie 5
+# Jaka zależność jest między liczbą koni mechanicznych a pojemnością skokową dla samochodów 
+# wyprodukowanych w 2007 i 2008 roku?
+
+
+library(SmarterPoland)
+
+## Skale 
+
+
+
+
+## Kolory
+
+
+
+
+# Legenda
+
+
+
+
+
+# Koordynaty (coord)
+
+
+
+
+
+# Panele (facet)
+
+
+
+
+
+library(PogromcyDanych)
+data("serialeIMDB")
+head(serialeIMDB)
+
+# Wykres liniowy
+
+
+
+
+
+# Heatmap
+
+
+
+
+
+
+
+## Zadanie 1
+# Jak zależy ocena od odcinka/sezonu/liczby głosów dla serialu *?
+# * - wybrany serial
+
+## Zadanie 2
+# Jaka jest różnica w ocenie seriali x i *?
+# x - serial z Zadania 1
+# * - drugi wybrany serial
