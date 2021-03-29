708e62620bba6f05c8bb671da689c0d62cd943cc
Anna Kozak <anna1993kozak@gmail.com>
2021-03-29 22:01:40 +0200

    add lab 6

---
+++ Laboratoria/Lab_6.R
@@ -0,0 +1,52 @@
+#########################################
+###    WSTĘP DO EKSPLORACJI DANYCH    ###
+###         LABORATORIUM 6            ###
+#########################################
+
+library(ggplot2)
+library(SmarterPoland)
+library(dplyr)
+
+# 0) Zadania z lab 5
+
+## Zadanie 1
+# Jak zależy ocena od odcinka/sezonu/liczby głosów dla serialu *?
+# * - wybrany serial
+
+## Zadanie 2
+# Jaka jest różnica w ocenie seriali x i *?
+# x - serial z Zadania 1
+# * - drugi wybrany serial
+
+
+# 1) Zaawansowane atrybuty  
+
+
+# 2) Transformacje danych
+
+install.packages("ggrepel")
+library(ggrepel)
+
+
+# 3) Wiele wykresow na jednym rysunku 
+
+
+## Wykresy gęstości brzegowych
+install.packages("gridExtra")
+library(gridExtra)
+
+install.packages("grid")
+library(grid)
+
+install.packages("patchwork")
+library(patchwork)
+
+
+# 4) ggstatsplot
+
+#https://github.com/IndrajeetPatil/ggstatsplot
+
+# 5) ggmap
+
+#https://github.com/dkahle/ggmap
+
