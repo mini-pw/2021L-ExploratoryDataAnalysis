d8a7235c3262557a9205bd00a22fa907bf616f68
Anna Kozak <anna1993kozak@gmail.com>
2021-04-18 19:40:34 +0200

    add lab 8

---
+++ Laboratoria/Lab_8.R
@@ -0,0 +1,41 @@
+#########################################
+###    WSTĘP DO EKSPLORACJI DANYCH    ###
+###         LABORATORIUM  8           ###
+#########################################
+
+# Wykresy interantywne w ploty 
+
+install.packages("plotly")
+
+library(plotly)
+library(tidyr)
+library(dyplr)
+
+install.packages("DALEX")
+library(DALEX)
+# Dane o smokach
+data("dragons")
+head(dragons)
+
+?dragons
+
+# https://plotly.com/r/
+
+# 1) wykres punktowy
+
+
+# 2) wykres liniowy
+
+
+# 3) wykres słupkowy
+
+
+# 4) boxplot
+
+
+# 5) histogram
+
+
+# 6) ggplotly
+
+
